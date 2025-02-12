c6afd4661cdf624ded1e9853181d0a176166adf1
VaniaGalimzianov <144653733+VaniaGalimzianov@users.noreply.github.com>
2025-02-12 16:04:23 +0300

    Update logProcess.sql

--- internship/logProcess.sql
+++ internship/logProcess.sql
@@ -1,14 +1,10 @@
-CREATE PROCEDURE dbo.LogProcess
-    @process_name NVARCHAR(255),
-    @handler NVARCHAR(255),
-    @error_message NVARCHAR(MAX) = NULL
+ALTER PROCEDURE dbo.LogProcess
+    @ProcessName NVARCHAR(255),
+    @Message NVARCHAR(MAX),
+	@StartTime DATETIME2,
+	@EndTime DATETIME2
 AS
 BEGIN
-    IF @error_message IS NULL
-        UPDATE dbo.Logs
-        SET end_time = GETDATE()
-        WHERE process_name = @process_name AND handler = @handler AND end_time IS NULL;
-    ELSE
-        INSERT INTO dbo.Logs (process_name, start_time, handler, error_message)
-        VALUES (@process_name, GETDATE(), @handler, @error_message);
+    INSERT INTO dbo.Logs (process_name, start_time, end_time, message)
+    VALUES (@ProcessName, @StartTime, @EndTime, @Message);
 END;
