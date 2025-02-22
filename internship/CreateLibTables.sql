1b49cef59e5947d8186cece253fcee2bbb2add8a
VaniaGalimzianov <144653733+VaniaGalimzianov@users.noreply.github.com>
2025-02-22 15:00:30 +0300

    Update CreateLibTables.sql

--- internship/CreateLibTables.sql
+++ internship/CreateLibTables.sql
@@ -7,3 +7,12 @@ CREATE TABLE dbo.GoodGroups (
     last_update_date DATETIME NOT NULL
 );
 
+CREATE TABLE dbo.Goods (
+    id INT PRIMARY KEY,
+    name NVARCHAR(255) NOT NULL,
+    manufacturer_id INT NULL,
+    barcodes NVARCHAR(100) NULL,
+    vat_percent DECIMAL(5,2) NULL,
+    created_date DATETIME NOT NULL,
+    last_update_date DATETIME NOT NULL
+);
