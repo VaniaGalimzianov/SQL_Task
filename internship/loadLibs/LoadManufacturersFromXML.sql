ef8722767216533e054b42b0c67ab9396dbcf45c
VaniaGalimzianov <144653733+VaniaGalimzianov@users.noreply.github.com>
2025-02-24 11:44:24 +0300

    Create LoadManufacturersFromXML.sql

---
+++ internship/loadLibs/LoadManufacturersFromXML.sql
@@ -0,0 +1,49 @@
+
+CREATE OR ALTER PROCEDURE LoadManufacturersFromXML
+    @XmlFilePath NVARCHAR(255)
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    DECLARE @XmlData XML;
+    DECLARE @SQL NVARCHAR(MAX);
+    DECLARE @LogError NVARCHAR(MAX);
+    DECLARE @StartTime DATETIME2 = SYSDATETIME();
+    DECLARE @EndTime DATETIME2;
+    DECLARE @ErrorMessage NVARCHAR(4000);
+
+    BEGIN TRY
+        -- Загружаем XML-файл в переменную
+        SET @SQL = 'SELECT @XmlData = CAST(BulkColumn AS XML)
+                    FROM OPENROWSET(BULK ''' + @XmlFilePath + ''', SINGLE_BLOB) AS x;';
+        EXEC sp_executesql @SQL, N'@XmlData XML OUTPUT', @XmlData OUTPUT;
+
+        -- Выполняем MERGE
+        MERGE INTO dbo.Manufacturers AS target
+        USING (
+            SELECT 
+                Item.value('(id)[1]', 'INT') AS id,
+                Item.value('(name)[1]', 'NVARCHAR(255)') AS name,
+                Item.value('(created_date)[1]', 'DATETIME') AS created_date,
+                Item.value('(last_update_date)[1]', 'DATETIME') AS last_update_date
+            FROM @XmlData.nodes('/root/manufacturers/manufacturer') AS ItemData(Item)
+        ) AS source
+        ON target.id = source.id
+        WHEN MATCHED THEN 
+            UPDATE SET 
+                target.name = source.name,
+                target.last_update_date = source.last_update_date
+        WHEN NOT MATCHED THEN
+            INSERT (id, name, created_date, last_update_date)
+            VALUES (source.id, source.name, source.created_date, source.last_update_date);
+
+        -- Логирование успешной загрузки
+        SET @EndTime = SYSDATETIME();
+        EXEC LogProcess 'LoadManufacturersFromXML', 'Successful data load into table Manufacturers', @StartTime, @EndTime;
+    END TRY
+    BEGIN CATCH
+        SET @ErrorMessage = ERROR_MESSAGE();
+        SET @LogError = 'Error: ' + @ErrorMessage;
+        EXEC LogProcess 'LoadManufacturersFromXML', @LogError, @StartTime, @EndTime;
+    END CATCH;
+END;
